0f396aa23f367824292e5a5b06852f1b445825ac
juani.scarafia <juani.scarafia@gmail.com>
2023-01-11 08:03:35 -0500

    remove netlify

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const serverless = require('serverless-http');
 const bodyParser = require("body-parser");
 const config = require("../config.js");
 const login = require("./components/login/network");
@@ -7,7 +6,6 @@ const measureMilks = require("./components/measuremilks/network");
 const errors = require("../network/errors");
 const err = require("../utils/error.js");
 const cors = require("cors");
-const router = express.Router();
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,14 +15,10 @@ app.use(cors());
 
 // RUTAS
 // app.use("/api/productos/Ec", productosEC);
-app.use('/.netlify/functions/index', router); 
-app.use("api/login", login);
-app.use("api/measuremilks", measureMilks);
+app.use("/api/login", login);
+app.use("/api/measuremilks", measureMilks);
 
 app.use(errors);
 app.listen(config.api.port, () => {
   console.log("Api escuchando en el puerto ", config.api.port);
 });
-
-module.exports = app;
-module.exports.handler = serverless(app);
